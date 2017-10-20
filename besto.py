4f712623ab0a81db6e2cbd18829ee236f99db9ff
40291478 <theciaranek@gmail.com>
2017-10-20 23:31:53 +0000

    All pages linked to and from the home page, information and .css to be done

--- besto.py
+++ besto.py
@@ -7,10 +7,35 @@ def main_page():
   image_names = os.listdir('./static')
   return render_template("main.html", image_names=image_names), 200
 
-@app.route("/eminem")
+@app.route("/eminem.jpg")
 def eminem_info():
+  filename = url_for('static' , filename='eminem.jpg')
+  return render_template("eminem.html", filename=filename)
+  
+@app.route("/fleetwood.jpg")
+def fleetwood_info():
+  filename = url_for('static' , filename='fleetwood.jpg')
+  return render_template("fleetwood.html", filename=filename)
 
-  return render_template("eminem.html", eminem.jpg)
+@app.route("/marvin.jpg")
+def marvin_info():
+  filename = url_for('static' , filename='marvin.jpg')
+  return render_template("marvin.html", filename=filename)
+  
+@app.route("/jackson.jpg")
+def jackson_info():
+  filename = url_for('static' , filename='jackson.jpg')
+  return render_template("jackson.html", filename=filename)
+  
+@app.route("/elvis.jpg")
+def elvis_info():
+  filename = url_for('static' , filename='elvis.jpg')
+  return render_template("elvis.html", filename=filename)
+
+@app.route("/aerosmith.jpg")
+def aerosmith_info():
+  filename = url_for('static' , filename='aerosmith.jpg')
+  return render_template("aerosmith.html", filename=filename)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
